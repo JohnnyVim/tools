9c501dfb36be91019083205c084bfa0f2005d744
JohnnyVim <32849404+JohnnyVim@users.noreply.github.com>
2019-07-09 14:45:31 +0200

    Add script to automate brew formula downgrades

---
+++ brew-downgrade.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly formula=$1
+readonly version=$2
+readonly formula_path="Formula/$formula.rb"
+
+if [ -z "$formula" ]; then
+  echo "./downgrade <formula_name> to list available versions"
+  echo "./downgrade <formula_name> <version> to install specified version"
+  exit 0
+fi
+
+cd "$(brew --repository homebrew/homebrew-core)"
+
+if [ ! -f "$formula_path" ]; then
+  echo "Unknown formula; Typo in \"$formula\"?"
+  exit 1
+fi
+
+if [ -z "$version" ]; then
+  git log --pretty=oneline --abbrev-commit "$formula_path" | grep -o '[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*' | sort -ruV | sed -n '$!p'
+  exit 0
+fi
+
+readonly brew_info=$(brew info "$formula" --json)
+readonly current_version=$(jq -r '.[].linked_keg' <<<"$brew_info")
+
+if [ "$current_version" = "$version" ]; then
+  echo "Version $version is already installed."
+  exit 0
+fi
+
+mapfile -t installed_version < <(jq -r '.[].installed[].version' <<<"$brew_info")
+for i in "${installed_version[@]}"; do
+  if [ "$i" = "$version" ]; then
+    echo "Switching to version $version"
+    brew switch "$formula" "$version"
+    brew pin "$formula" >/dev/null
+    exit 0
+  fi
+done
+
+readonly commit=$(git log --pretty=oneline --abbrev-commit "$formula_path" | sed -n "/${version//./\.}/{p;q;}" | cut -d ' ' -f 1)
+git checkout "$commit" "$formula_path"
+brew unlink "$formula"
+brew install "$formula"
+brew pin "$formula" >/dev/null
+git checkout master "$formula_path"
